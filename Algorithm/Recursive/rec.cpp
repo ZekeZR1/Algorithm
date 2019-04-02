0283395865806e0a94573c3abcd820393b4db44b
Zeke <hnta3574@gmail.com>
2019-04-02 21:24:58 +0900

    rec training

--- Algorithm/Recursive/rec.cpp
+++ Algorithm/Recursive/rec.cpp
@@ -18,6 +18,19 @@ int sum_range(int a, int b) {
 	return sum_range(a + 1, b) + a;
 }
 
+vector<int> reverse_array_from_i(vector<int>& data, int i) {
+	if (data.size() == i) {
+		vector<int> empty_array(0);
+		return empty_array;
+	}
+	vector<int> tmp = reverse_array_from_i(data, i + 1);
+	tmp.push_back(data.at(i));
+	return tmp;
+}
+
+vector<int> reverse_array(vector<int>& data) {
+	return reverse_array_from_i(data, 0);
+}
 
 int array_sum_from_i(vector<int>& data, int i) {
 	if (i == data.size())
@@ -29,11 +42,32 @@ int array_sum(vector<int> &data) {
 	return array_sum_from_i(data, 0);
 }
 
+//n >=3
+bool has_divisor(int n, int i) {
+	if (n % i == 0)
+		return true;
+	if (i == n - 1)
+		return false;
+	return has_divisor(n, i + 1);
+}
+
+bool isPrime(int n) {
+	if (n == 1) {
+		return false;
+	}
+	else if (n == 2) {
+		return true;
+	}
+	return !has_divisor(n, 2);
+}
+
 int main()
 {
-	vector<int> a = { 0, 3, 9, 1, 5 };
-	cout << array_sum(a) << endl;   // 0 + 3 + 9 + 1 + 5 = 18
-
+	vector<int> a = { 1, 2, 3, 4, 5 };
+	vector<int> b = reverse_array(a);
+	for (int i = 0; i < b.size(); i++) {
+		cout << b.at(i) << endl;
+	}
 	return 0;
 }
 
