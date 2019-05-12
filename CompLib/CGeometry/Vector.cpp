b29842db86a73101b7fcf625750ee5f83ef58200
Zeke <hnta3574@gmail.com>
2019-05-12 21:41:02 +0900

    add segment distance getter func and cross judge

--- CompLib/CGeometry/Vector.cpp
+++ CompLib/CGeometry/Vector.cpp
@@ -1,5 +1,11 @@
 ﻿#define equals(a,b) (fabs((a) - (b)) < EPS)
 
+static const int COUNTER_CLOCKWISE = 1;
+static const int CLOCKWISE = -1;
+static const int ONLINE_BACK = 2;
+static const int ONLINE_FRONT = -2;
+static const int ON_SEGMENT = 0;
+
 class Point {
 public:
 	double x, y;
@@ -24,7 +30,6 @@ public:
 };
 
 typedef Point Vector;
-typedef Segment Line;
 typedef vector<Point> Polygon;
 
 double dot(Vector a, Vector b) {
@@ -40,6 +45,8 @@ public:
 	Point p1, p2;
 	Segment(Point p1, Point p2) : p1(p1), p2(p2) {}
 };
+
+typedef Segment Line;
 
 class Circle {
 public:
@@ -93,7 +100,7 @@ double getDistance(Point a, Point b) {
 }
 //点と直線の距離
 double getDistanceLP(Line l, Point p) {
-	return abs(cross(l.p2 - l.p1, p - l.p1) / abs(l.p2 - l.p1));
+	return abs(cross(l.p2 - l.p1, p - l.p1) / Vector(l.p2 - l.p1).abs());
 }
 
 //線分ｓと点ｐの距離
@@ -102,11 +109,31 @@ double getDistanceSP(Segment s, Point p) {
 		Point r = p - s.p1;
 		return r.abs();
 	}
+	if (dot(s.p1 - s.p2, p - s.p2) < 0.0) {
+		Point r = p - s.p2;
+		return r.abs();
+	}
+	return getDistanceLP(s, p);
+}
+
+//counter clock wise
+int ccw(Point p0, Point p1, Point p2) {
+	Vector a = p1 - p0;
+	Vector b = p2 - p0;
+	if (cross(a, b) > EPS) return COUNTER_CLOCKWISE;
+	if (cross(a, b) < -EPS) return CLOCKWISE;
+	if (dot(a, b) < -EPS) return ONLINE_BACK;
+	if (a.norm() < b.norm()) return ONLINE_FRONT;
+	return ON_SEGMENT;
 }
 
 //線分と線分の交差判定
+bool intersect(Point p1, Point p2, Point p3, Point p4) {
+	return (ccw(p1, p2, p3) * ccw(p1, p2, p4) <= 0 and
+		ccw(p3, p4, p1) * ccw(p3, p4, p2) <= 0);
+}
 bool intersect(Segment s1, Segment s2) {
-	//return ( ccw())
+	return intersect(s1.p1, s1.p2, s2.p1, s2.p2);
 }
 
 //線分と線分の距離
@@ -116,4 +143,13 @@ double getDistance(Segment s1, Segment s2) {
 		min(getDistanceSP(s1, s2.p1), getDistanceSP(s1, s2.p2)),
 		min(getDistanceSP(s2, s1.p1), getDistanceSP(s2, s1.p2))
 	);
+}
+
+//線分s1と線分s2の交点
+Point getCrossPoint(Segment s1, Segment s2) {
+	Vector base = s2.p2 - s2.p1;
+	double d1 = abs(cross(base, s1.p1 - s2.p1));
+	double d2 = abs(cross(base, s1.p2 - s2.p1));
+	double t = d1 / (d1 + d2);
+	return s1.p1 + (s1.p2 - s1.p1) * t;
 }
